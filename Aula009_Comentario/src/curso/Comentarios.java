fb4d6a59ebb0f3baf6fc1a1efa88ec6f211ee7ef
Gerson Santos <gersones@outlook.com>
2023-12-18 02:43:51 -0300

    a

---
+++ Aula009_Comentario/src/curso/Comentarios.java
@@ -0,0 +1,20 @@
+/**
+ * prog
+ * @author gerso
+ * @ver
+ * 
+ */
+package curso;
+
+public class Comentarios {
+
+	public static void main(String[] args) {
+		// Comentarios
+		System.out.println("Primeira Linha");
+		System.out.println("segunda Linha");
+		System.out.println("terceira Linha");
+		
+		
+	}
+
+}
