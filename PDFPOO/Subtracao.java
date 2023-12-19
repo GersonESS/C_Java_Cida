d92ba331b1ce14491879c61b63e20cd8d601feb3
Gerson Santos <gersones@outlook.com>
2023-12-19 07:56:32 -0300

    Update Subtracao.java

--- PDFPOO/Subtracao.java
+++ PDFPOO/Subtracao.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
+
 public class Subtracao
 {
-public static void main(String args [ ])
- {
-Scanner estrada = new Scanner (System. in);
-int numero1, numero2, subtracao;
-System.out.printf("Informe o primeiro número\n");
-numero1 = entrada.next( ); //lê o primeiro número
-System.out.printf("Informe o segundo número\n");
-numero2 = entrada.nextInt( ); //lê o segundo número
-subtracao = numero1-numero2;
-System.out.printf ("A subtração é %d\n", subtracao);
-}
+    public static void main(String args [ ])
+    {
+    
+        int numero1, numero2, subtracao;
+        System.out.printf("Informe o primeiro número\n");
+        numero1 = entrada.next( ); //lê o primeiro número
+        System.out.printf("Informe o segundo número\n");
+        numero2 = entrada.nextInt( ); //lê o segundo número
+        subtracao = numero1  - numero2;
+        System.out.printf ("A subtração é %d\n", subtracao);
+    }
 }
