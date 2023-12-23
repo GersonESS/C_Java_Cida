427b2021a30658d3f3d37af6943f950560a77887
Gerson Santos <gersones@outlook.com>
2023-12-23 16:55:16 -0300

    a

--- Aula014_Rxercicio_0304/src/curso/EX03NomeIdade.java
+++ Aula014_Rxercicio_0304/src/curso/EX03NomeIdade.java
@@ -1,9 +1,30 @@
 package curso;
 
+import java.util.Scanner;
+
 public class EX03NomeIdade {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//instanciar um objeto da classe Scanner
+		//criar um objeto
+		Scanner sc = new Scanner(System.in);
+		//variáveis
+		String nome;
+		String sobrenome;
+		int idade;
+		
+		//entrada de dados
+		System.out.print("Digite seu nome: ");
+		nome = sc.nextLine();
+		
+		System.out.print("Digite seu sobrenome: ");
+		sobrenome = sc.nextLine();	
+		System.out.print("Digite sua idade: ");
+		idade = sc.nextInt();
+		
+		//saída de dados
+		System.out.println("Seu nome é " + nome + " " + sobrenome +
+				" e você tem " + idade + " anos" );
 
 	}
 
