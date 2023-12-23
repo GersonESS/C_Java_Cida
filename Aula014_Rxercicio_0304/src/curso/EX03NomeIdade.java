36715e0ae4c44fe6599d12354feab599b9ca3068
Gerson Santos <gersones@outlook.com>
2023-12-23 16:44:15 -0300

    a

---
+++ Aula014_Rxercicio_0304/src/curso/EX03NomeIdade.java
@@ -0,0 +1,10 @@
+package curso;
+
+public class EX03NomeIdade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Aula014_Rxercicio_0304/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Aula014_Rxercicio_0304 {
+}
