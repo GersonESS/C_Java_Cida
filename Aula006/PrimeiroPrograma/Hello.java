cb69ff7a4b3511324c624a4b693c62a24d8f2f84
Gerson Santos <gersones@outlook.com>
2024-04-12 21:31:35 -0300

    a

--- Aula006/PrimeiroPrograma/Hello.java
+++ Aula006/PrimeiroPrograma/Hello.java
@@ -2,11 +2,8 @@ public class Hello{
 	
 	public static void main(String[] args){
 		
-<<<<<<< HEAD
 		System.out.println("Hello, Worldssds2!");
-=======
 		System.out.println("Hello, Worlds no linuz!");
->>>>>>> 0f065056d96b1d30208ebcb1839bbf731db6d477
 		
 	}
 }
